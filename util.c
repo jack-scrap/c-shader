0f62954a1c141ecc25bb16b823e959cd216c2e8b
Jack <jackhasakeyboard@gmail.com>
2023-10-20 09:31:36 -0600

    Created utility to convert scalar index to a 2D coordinate

--- util.c
+++ util.c
@@ -6,6 +6,15 @@ unsigned int coordToIdx(Coord st, Coord bound) {
 	return (st._y * bound._x) + st._x;
 }
 
+Coord idxToCoord(unsigned int i, Coord bound) {
+	Coord st = {
+		i % bound._x,
+		i % bound._y
+	};
+
+	return st;
+}
+
 int inRng(int val, int floor, int roof) {
 	return val >= floor && val <= roof;
 }
--- util.h
+++ util.h
@@ -5,6 +5,8 @@
 
 unsigned int coordToIdx(Coord st, Coord bound);
 
+Coord idxToCoord(unsigned int i, Coord bound);
+
 int inRng(int val, int floor, int roof);
 
 float norm(unsigned int val, axis_t axis);
